90412c14dd39ef2ea7eec20fc0c571f1adf3f564
SongJaeHoonn <thdwogns121@naver.com>
2024-10-24 16:27:18 +0900

    refactor: 회원별 알림 전송 기능 추가

--- src/main/java/api/kgu/nufarm/application/notification/entity/Notification.java
+++ src/main/java/api/kgu/nufarm/application/notification/entity/Notification.java
@@ -1,12 +1,16 @@
 package api.kgu.nufarm.application.notification.entity;
 
+import api.kgu.nufarm.application.user.entity.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -30,15 +34,20 @@ public class Notification {
     @Column(nullable = false)
     private String userItem;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
     @Enumerated(EnumType.STRING)
     private NotificationType type;
 
     @Column(nullable = false)
     private LocalDateTime time;
 
-    public static Notification of(NotificationType type, String userItem, LocalDateTime time) {
+    public static Notification of(NotificationType type, String userItem, User user, LocalDateTime time) {
         return Notification.builder()
                 .userItem(userItem)
+                .user(user)
                 .type(type)
                 .time(time)
                 .build();
